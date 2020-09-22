8fe2a94d271db9a050238ced32b0ae31b3e5a357
JordanC <jcwilliams410@gmail.com>
2020-09-21 21:26:00 -0400

    demo video added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,35 @@
     <h1 id="header-text">CINEFAVORITES</h1>
     <h2 id="subheader-text">All your favorite movies in one place.</h2>
     <video class="video" autoplay loop playsinline muted>
-            <source src="./demo.mp4" type="video/mp4">
+            <source src="./demo.mp4">
             Your browser does not support the video tag.
     </video>
-    <b-carousel id="carousel" >
-        <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
-            <section :class="`hero is-medium is-${carousel.color}`">
-                <div class="hero-body has-text-centered">
-                    <h1 class="title">{{carousel.text}}</h1>
-                </div>
-            </section>
+    <b-carousel
+        :autoplay="false"
+        with-carousel-list
+        :indicator="false"
+        :overlay="gallery"
+        @click="switchGallery(true)" class="test">
+        <b-carousel-item v-for="(item, i) in items" :key="i">
+            <figure class="image">
+                <img :src="item.image">
+                <h1>WHOOOOP</h1>
+            </figure>
         </b-carousel-item>
+        <span v-if="gallery" @click="switchGallery(false)" class="modal-close is-large"/>
+        <template slot="list" slot-scope="props">
+            <b-carousel-list
+                v-model="props.active"
+                :data="items"
+                v-bind="al"
+                @switch="props.switch($event, false)"
+                as-indicator />
+        </template>
+        <template slot="overlay">
+            <div class="has-text-centered has-text-white">
+                Hello i'am overlay!
+            </div>
+        </template>
     </b-carousel>
   </div>
 </template>
@@ -27,15 +45,51 @@ export default {
   components: {
     
   },
-  data(){
+  data() {
         return {
-            carousels: [
-                { text: 'Slide 1', color: 'primary' },
-                { text: 'Slide 2', color: 'info' },
-                { text: 'Slide 3', color: 'success' },
-                { text: 'Slide 4', color: 'warning' },
-                { text: 'Slide 5', color: 'danger' }
+            gallery: false,
+            al: {
+                hasGrayscale: true,
+                itemsToShow: 2,
+                breakpoints: {
+                    768: {
+                        hasGrayscale: false,
+                        itemsToShow: 4
+                    },
+                    960: {
+                        hasGrayscale: true,
+                        itemsToShow: 6
+                    }
+                }
+            },
+            items: [
+                {
+                    title: 'Slide 1',
+                    image: 'https://res.cloudinary.com/dpjdvsigb/image/upload/v1600737726/project-4-assets/third-carousel-image_znrdxk.jpg'
+                },
+                {
+                    title: 'Slide 2',
+                    image: 'https://res.cloudinary.com/dpjdvsigb/image/upload/v1600732087/project-4-assets/first-carousel-image_gaewop.jpg'
+                },
+                {
+                    title: 'Slide 3',
+                    image: 'https://res.cloudinary.com/dpjdvsigb/image/upload/v1600737725/project-4-assets/forth-carousel-image_b5w1wa.jpg'
+                },
+                {
+                    title: 'Slide 4',
+                    image: 'https://res.cloudinary.com/dpjdvsigb/image/upload/v1600732099/project-4-assets/forth-carousel-image_plht0l.png'
+                }
             ]
+        }
+    },
+    methods: {
+        switchGallery(value) {
+            this.gallery = value
+            if (value) {
+                document.documentElement.classList.add('is-clipped')
+            } else {
+                document.documentElement.classList.remove('is-clipped')
+            }
         }
     }
 }
@@ -60,7 +114,8 @@ export default {
     width: 50%;
     min-width: 400px;
   }
-  #carousel {
+
+  .test {
     width: 50%;
     min-width: 400px;
     margin: 0 auto;
